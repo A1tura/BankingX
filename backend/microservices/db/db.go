d0f2493944dca5fe131f9e567d62630d533434c6
A1tura <plandik275@gmail.com>
2025-03-31 18:18:17 +0200

    db connection lib implemented

---
+++ backend/microservices/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type DB struct {
+	*sql.DB
+}
+
+func Connect(user, password, db string) *DB {
+	con, err := sql.Open("postgres", "user="+user+" password="+password+" dbname="+db+" port=5432 sslmode=disable")
+	if err != nil {
+		fmt.Errorf("Error while connecting to DB: ", err.Error())
+	}
+
+	if err := con.Ping(); err != nil {
+		fmt.Errorf("Error while pining DB: ", err.Error())
+	}
+
+	return &DB{
+		DB: con,
+	}
+}
