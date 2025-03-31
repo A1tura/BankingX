9b6a4dd041fe25add6bd6c4e10baa54c2d3d2ae3
A1tura <plandik275@gmail.com>
2025-03-31 18:18:41 +0200

    middlewares lib implemented

---
+++ backend/microservices/middlewares/go.mod
@@ -0,0 +1,9 @@
+module middlewares
+
+go 1.23.4
+
+replace db => ../db
+
+require db v0.0.0-00010101000000-000000000000
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ backend/microservices/middlewares/middlewares.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import (
+	"context"
+	"db"
+	"net/http"
+)
+
+func GetMiddleware(db *db.DB) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), "db", db)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
