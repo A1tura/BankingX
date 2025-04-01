adeab3f876a46f285382122f7de4ce6f2efe13d7
A1tura <plandik275@gmail.com>
2025-04-01 14:35:44 +0200

    refactor: return error instead of bool and implemented: UserExist and GetUserId functions

--- backend/microservices/user/dal/dal.go
+++ backend/microservices/user/dal/dal.go
@@ -2,10 +2,9 @@ package dal
 
 import (
 	"db"
-	"fmt"
 )
 
-func EmailInUse(db *db.DB, email string) (bool, bool) {
+func EmailInUse(db *db.DB, email string) (bool, error) {
 	var emailInUse bool
 	row := db.QueryRow(`SELECT CASE
            WHEN EXISTS (SELECT 1 FROM users WHERE email=$1) THEN true
@@ -13,14 +12,13 @@ func EmailInUse(db *db.DB, email string) (bool, bool) {
        END;`, email)
 
 	if err := row.Scan(&emailInUse); err != nil {
-		fmt.Println(err)
-		return false, false
+		return false, err
 	}
 
-	return true, emailInUse
+	return emailInUse, nil
 }
 
-func UsernameInUse(db *db.DB, email string) (bool, bool) {
+func UsernameInUse(db *db.DB, email string) (bool, error) {
 	var usernameInUse bool
 	row := db.QueryRow(`SELECT CASE
            WHEN EXISTS (SELECT 1 FROM users WHERE username=$1) THEN true
@@ -28,20 +26,45 @@ func UsernameInUse(db *db.DB, email string) (bool, bool) {
        END;`, email)
 
 	if err := row.Scan(&usernameInUse); err != nil {
-		fmt.Println(err)
-		return false, false
+		return false, err
 	}
 
-	return true, usernameInUse
+	return usernameInUse, nil
 }
 
-func CreateUser(db *db.DB, username, password_hash, email string) (bool, int) {
+func CreateUser(db *db.DB, username, password_hash, email string) (int, error) {
 	var id int
 	row := db.QueryRow(`INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING id;`, username, email, password_hash)
 
 	if err := row.Scan(&id); err != nil {
-		return false, 0
+		return 0, err
 	}
 
-	return true, id
+	return id, nil
+}
+
+func UserExist(db *db.DB, email, password_hash string) (bool, error) {
+	var exist bool
+	row := db.QueryRow(`SELECT CASE
+           WHEN EXISTS (SELECT 1 FROM users WHERE email=$1 AND password_hash=$2) THEN true
+           ELSE false
+       END;`, email, password_hash)
+
+	if err := row.Scan(&exist); err != nil {
+		return false, err
+	}
+
+	return exist, nil
+}
+
+func GetUserId(db *db.DB, email string) (int, error) {
+	var id int
+
+	row := db.QueryRow(`SELECT id FROM users WHERE email=$1`, email)
+
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
