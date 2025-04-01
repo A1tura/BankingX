81dc31eb6f6e70b12cac483cdf7b6609ce0fcd7e
A1tura <plandik275@gmail.com>
2025-04-01 12:18:22 +0200

    implemented: usernameInUse function

--- backend/microservices/user/dal/dal.go
+++ backend/microservices/user/dal/dal.go
@@ -12,10 +12,25 @@ func EmailInUse(db db.DB, email string) (bool, bool) {
            ELSE false
        END;`, email)
 
-    if err := row.Scan(&emailInUse); err != nil {
-        fmt.Println(err)
-        return false, false
-    }
+	if err := row.Scan(&emailInUse); err != nil {
+		fmt.Println(err)
+		return false, false
+	}
 
 	return true, emailInUse
+}
+
+func UsernameInUse(db db.DB, email string) (bool, bool) {
+	var usernameInUse bool
+	row := db.QueryRow(`SELECT CASE
+           WHEN EXISTS (SELECT 1 FROM users WHERE username=$1) THEN true
+           ELSE false
+       END;`, email)
+
+	if err := row.Scan(&usernameInUse); err != nil {
+		fmt.Println(err)
+		return false, false
+	}
+
+	return true, usernameInUse
 }
