f76e0ac24b4506367cd157265175ce6d53a3999f
A1tura <plandik275@gmail.com>
2025-04-02 15:13:55 +0200

    implemented: listen for messages in email channel

--- backend/microservices/email/main.go
+++ backend/microservices/email/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"email/rabbitmq"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+    godotenv.Load()
+    if err := rabbitmq.Listen(); err != nil {
+        log.Fatal(err)
+    }
 }
