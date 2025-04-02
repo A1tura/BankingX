65ae3a6e1dbaf5778b3e4e245b24054cc6d4d285
A1tura <plandik275@gmail.com>
2025-04-02 14:02:23 +0200

    implemented: templater

---
+++ backend/microservices/email/templates/templates.go
@@ -0,0 +1,58 @@
+package templates
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Template struct {
+	Subject string `json:"subject"`
+	Message string `json:"message"`
+	Sender  string `json:"sender"`
+}
+
+var templates map[string]Template
+
+func LoadTemplates() error {
+	file, err := os.Open("./templates/templates.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(content, &templates); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ApplyTemplate(templateName string, args map[string]string) (Template, error) {
+	if err := LoadTemplates(); err != nil {
+		return Template{}, err
+	}
+
+	res := Template{}
+
+	template := templates[templateName]
+    msg := template.Message
+
+    for key, value := range args {
+        placeholder := fmt.Sprintf("{{.%s}}", key)
+        msg = strings.ReplaceAll(msg, placeholder, value)
+    }
+
+    res.Message = msg
+    res.Sender = template.Sender
+    res.Subject = template.Subject
+
+	return res, nil
+}
