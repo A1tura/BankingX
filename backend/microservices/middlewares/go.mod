31c61b3e761d1484ec5abb16831b4069ccb74a8d
A1tura <plandik275@gmail.com>
2025-04-02 12:31:56 +0200

    implemented: struct to store all the services

--- backend/microservices/middlewares/go.mod
+++ backend/microservices/middlewares/go.mod
@@ -6,4 +6,7 @@ replace db => ../db
 
 require db v0.0.0-00010101000000-000000000000
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+)
--- backend/microservices/middlewares/middlewares.go
+++ backend/microservices/middlewares/middlewares.go
@@ -3,20 +3,33 @@ package middlewares
 import (
 	"context"
 	"db"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"net/http"
 )
 
-func GetMiddleware(db *db.DB) func(next http.Handler) http.Handler {
+type Services struct {
+	DB       *db.DB
+	Rabbitmq *amqp.Connection
+}
+
+func GetMiddleware(db *db.DB, rabbitmq *amqp.Connection) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(r.Context(), "db", db)
+			ctx = context.WithValue(ctx, "rabbitmq", rabbitmq)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
 }
 
-func GetContext(ctx context.Context) *db.DB {
+func GetContext(ctx context.Context) *Services {
 	db := ctx.Value("db").(*db.DB)
+	rabbtimq := ctx.Value("rabbitmq").(*amqp.Connection)
 
-	return db
+	services := Services{
+		DB:       db,
+		Rabbitmq: rabbtimq,
+	}
+
+	return &services
 }
