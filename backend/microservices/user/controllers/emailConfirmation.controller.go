2e8a19e2568ef3ca7cb8f7eaf50857f6dc15747a
A1tura <plandik275@gmail.com>
2025-04-02 17:37:12 +0200

    implemented: /emailConfirmation endpoint

--- backend/microservices/user/controllers/emailConfirmation.controller.go
+++ backend/microservices/user/controllers/emailConfirmation.controller.go
@@ -2,13 +2,44 @@ package controllers
 
 import (
 	"fmt"
+	"middlewares"
 	"net/http"
+	"user/dal"
+	"user/error"
 )
 
 func EmailConfirmation(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodGet {
-        fmt.Fprint(w, "/emailConfirmation endpoint")
-    } else {
-        w.WriteHeader(404)
-    }
+	if r.Method == http.MethodGet {
+		token := r.URL.Query().Get("token")
+		error := error.NewError(true, w)
+		services := middlewares.GetContext(r.Context())
+
+		if token == "" {
+			error.NewError("Invlaid or Expired token")
+            error.ThrowError()
+            return
+		}
+
+		successful, err := dal.VerifyToken(services.DB, token)
+		if err != nil {
+			error.ThrowInternalError()
+			return
+		}
+
+		if !successful {
+            error.NewError("Invalid or Expired token")
+            error.ThrowError()
+            return
+		}
+
+		if error.ErrorsExist() {
+			error.ThrowError()
+			return
+		} else {
+			fmt.Fprint(w, "Nice")
+		}
+
+	} else {
+		w.WriteHeader(404)
+	}
 }
