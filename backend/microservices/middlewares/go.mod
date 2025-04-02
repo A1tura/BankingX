d10f388e611f77d6b8b0f7c355a272d217358664
A1tura <plandik275@gmail.com>
2025-04-02 22:39:12 +0200

    implemented: jwt token verification and decoding

--- backend/microservices/middlewares/go.mod
+++ backend/microservices/middlewares/go.mod
@@ -7,6 +7,7 @@ replace db => ../db
 require db v0.0.0-00010101000000-000000000000
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
 )
--- backend/microservices/middlewares/middlewares.go
+++ backend/microservices/middlewares/middlewares.go
@@ -3,8 +3,12 @@ package middlewares
 import (
 	"context"
 	"db"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type Services struct {
@@ -12,14 +16,70 @@ type Services struct {
 	Rabbitmq *amqp.Connection
 }
 
+type AuthInfo struct {
+	IsAuth bool
+	UserId int
+}
+
 func GetMiddleware(db *db.DB, rabbitmq *amqp.Connection) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(r.Context(), "db", db)
 			ctx = context.WithValue(ctx, "rabbitmq", rabbitmq)
+
+			authHeader := strings.Split(r.Header.Get("Authorization"), " ")
+
+			if authHeader[0] == "Bearer" {
+				token, err := jwt.Parse(authHeader[1], func(token *jwt.Token) (interface{}, error) {
+					return []byte(os.Getenv("JWT_SECRET")), nil
+				})
+
+				if err != nil {
+					ctx = context.WithValue(ctx, "isAuth", false)
+					next.ServeHTTP(w, r.WithContext(ctx))
+					return
+				}
+
+				if !token.Valid {
+					ctx = context.WithValue(ctx, "isAuth", false)
+					next.ServeHTTP(w, r.WithContext(ctx))
+					return
+				}
+
+				claims, ok := token.Claims.(jwt.MapClaims)
+				if !ok {
+					ctx = context.WithValue(ctx, "isAuth", false)
+					next.ServeHTTP(w, r.WithContext(ctx))
+					return
+				}
+				userId := claims["userId"].(float64)
+
+				ctx = context.WithValue(ctx, "isAuth", true)
+				ctx = context.WithValue(ctx, "userId", int(userId))
+			}
+
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
+}
+
+func GetAuth(ctx context.Context) *AuthInfo {
+	isAuthValue := ctx.Value("isAuth")
+	userIdValue := ctx.Value("userId")
+
+	if isAuthValue == nil || userIdValue == nil {
+		return &AuthInfo{IsAuth: false, UserId: 0}
+	}
+
+    isAuth := isAuthValue.(bool)
+    userId := userIdValue.(int)
+
+	authInfo := AuthInfo{
+		IsAuth: isAuth,
+		UserId: userId,
+	}
+
+	return &authInfo
 }
 
 func GetContext(ctx context.Context) *Services {
