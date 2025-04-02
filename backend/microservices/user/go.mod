f80a923c6f76e6d07766d44f33d4aa414698e3ed
A1tura <plandik275@gmail.com>
2025-04-02 12:37:34 +0200

    implemented: rabbitmq to user microservice

--- backend/microservices/user/go.mod
+++ backend/microservices/user/go.mod
@@ -2,14 +2,15 @@ module user
 
 go 1.23.4
 
-require middlewares v0.0.0-00010101000000-000000000000
-
 require (
-	db v0.0.0-00010101000000-000000000000 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	db v0.0.0-00010101000000-000000000000
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/joho/godotenv v1.5.1
+	github.com/rabbitmq/amqp091-go v1.10.0
+	middlewares v0.0.0-00010101000000-000000000000
 )
+
+require github.com/lib/pq v1.10.9 // indirect
 
 replace middlewares => ../middlewares
 
--- backend/microservices/user/main.go
+++ backend/microservices/user/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"db"
+	"log"
 	"middlewares"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
 
 	"user/controllers"
 )
 
-var con = db.Connect("admin", "admin", "bank")
-var ctxMiddleware = middlewares.GetMiddleware(con)
-
 func main() {
 	godotenv.Load()
+
+	con := db.Connect("admin", "admin", "bank")
+	rabbitmq, err := amqp.Dial(os.Getenv("RABBITMQ"))
+	if err != nil {
+		log.Fatalf("Error while connecting to rabbitmq: ", err)
+	}
+	var ctxMiddleware = middlewares.GetMiddleware(con, rabbitmq)
+
 	http.Handle("/signup", ctxMiddleware(http.HandlerFunc(controllers.SignUp)))
 	http.Handle("/signin", ctxMiddleware(http.HandlerFunc(controllers.SignIn)))
 
