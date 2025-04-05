8f5e9cd1f8abdd24dfd80e0d379d636c9c1e21d5
A1tura <plandik275@gmail.com>
2025-04-05 14:21:12 +0200

    implemented: basic microservice setup

--- backend/microservices/KYC/go.mod
+++ backend/microservices/KYC/go.mod
@@ -1,3 +1,19 @@
 module kyc
 
 go 1.23.4
+
+replace db => ../db
+
+replace middlewares => ../middlewares
+
+require (
+	db v0.0.0-00010101000000-000000000000
+	github.com/joho/godotenv v1.5.1
+	middlewares v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+)
---
+++ backend/microservices/KYC/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"db"
+	"kyc/controllers"
+	"log"
+	"middlewares"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	godotenv.Load()
+
+	db := db.Connect(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), os.Getenv("DB_HOST"))
+    rabbitmq, err := amqp.Dial(os.Getenv("RABBITMQ"))
+    if err != nil {
+        log.Fatalf("Error while connecting to rabbitmq: ", rabbitmq)
+    }
+
+    middleware := middlewares.GetMiddleware(db, rabbitmq)
+
+    http.Handle("/test", middleware(http.HandlerFunc(controllers.Test)))
+
+    http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+}
