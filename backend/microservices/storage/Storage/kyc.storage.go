583f736a6bbec595456fd518f887ded5a43ebbf4
A1tura <plandik275@gmail.com>
2025-04-08 23:39:54 +0200

    implemented: function to sumbit selfies

--- backend/microservices/storage/Storage/kyc.storage.go
+++ backend/microservices/storage/Storage/kyc.storage.go
@@ -8,18 +8,18 @@ import (
 	"github.com/minio/minio-go/v7"
 )
 
-func UploadDocument(minioClient *minio.Client, documentType string, document []byte, userId int) error {
+func UploadDocument(minioClient *minio.Client, documentType string, document []byte, userId int) (string, error) {
 	found, err := minioClient.BucketExists(context.Background(), "kyc-documents")
 	if !found {
 		err := minioClient.MakeBucket(context.Background(), "kyc-documents", minio.MakeBucketOptions{
 			Region: "us",
 		})
 		if err != nil {
-			return err
+			return "", err
 		}
 	}
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	image := bytes.NewReader(document)
@@ -27,8 +27,33 @@ func UploadDocument(minioClient *minio.Client, documentType string, document []b
 	filename := fmt.Sprint(userId, "/", documentType, ".png")
 	_, err = minioClient.PutObject(context.Background(), "kyc-documents", filename, image, int64(image.Len()), minio.PutObjectOptions{})
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return filename, nil
+}
+
+func UploadSelfie(minioClient *minio.Client, selfie []byte, userId int) (string, error) {
+	found, err := minioClient.BucketExists(context.Background(), "kyc-documents")
+	if !found {
+		err := minioClient.MakeBucket(context.Background(), "kyc-documents", minio.MakeBucketOptions{
+			Region: "us",
+		})
+		if err != nil {
+			return "", err
+		}
+	}
+	if err != nil {
+		return "", err
+	}
+
+	image := bytes.NewReader(selfie)
+
+	filename := fmt.Sprint(userId, "/selfie.png")
+	_, err = minioClient.PutObject(context.Background(), "kyc-documents", filename, image, int64(image.Len()), minio.PutObjectOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	return filename, nil
 }
