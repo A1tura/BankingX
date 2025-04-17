72b39232da331d6508910a07670e3525056c2fc2
A1tura <plandik275@gmail.com>
2025-04-17 11:30:43 +0200

    implemented: redis

--- backend/microservices/user/main.go
+++ backend/microservices/user/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/joho/godotenv"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/redis/go-redis/v9"
 	"github.com/rs/cors"
 
 	"user/controllers"
@@ -17,12 +18,22 @@ import (
 func main() {
 	godotenv.Load()
 
+	log.Println(os.Getenv("REDIS_ADDRESS"))
+
+	redis := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB: 1,
+	})
+
+
 	con := db.Connect(os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), os.Getenv("DB_HOST"))
 	rabbitmq, err := amqp.Dial(os.Getenv("RABBITMQ"))
 	if err != nil {
 		log.Fatalf("Error while connecting to rabbitmq: ", err)
 	}
 	var ctxMiddleware = middlewares.GetMiddleware(con, rabbitmq)
+	ctxMiddleware = middlewares.AddMiddleware(ctxMiddleware, "redis", redis)
 
 	http.Handle("/signup", ctxMiddleware(http.HandlerFunc(controllers.SignUp)))
 	http.Handle("/signin", ctxMiddleware(http.HandlerFunc(controllers.SignIn)))
